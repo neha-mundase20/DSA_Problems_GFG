3e0a109313a0ce1b3c61d77368f24374e825cced
Neha Mundase <106380291+neha-mundase20@users.noreply.github.com>
2024-08-12 18:26:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        stack<char>stk;
+        for(int i=0;x[i]!='\0';i++){
+            if(x[i]=='(' || x[i]=='{' || x[i]=='['){
+                stk.push(x[i]);
+            }
+            else{
+                if(stk.empty()){
+                    return false;
+                }
+                else{
+                   if((stk.top()=='(' && x[i]==')') || (stk.top()=='{' && x[i]=='}')
+                    || (stk.top()=='[' && x[i]==']')){
+                        stk.pop();
+                    }
+                    else{
+                        return false;   //mismatched closing brackets
+                    }
+                }
+            }
+        }
+        return stk.empty();
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+   int t;
+   string a;
+   cin>>t;
+   while(t--)
+   {
+       cin>>a;
+       Solution obj;
+       if(obj.ispar(a))
+        cout<<"balanced"<<endl;
+       else
+        cout<<"not balanced"<<endl;
+   }
+}
+// } Driver Code Ends
